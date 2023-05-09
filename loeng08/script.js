47fdec223a8b36616cf9ab94bed1e7ae234b85cb
KasparK1 <130385209+KasparK1@users.noreply.github.com>
2023-05-09 15:35:27 +0300

    kommit 09

---
+++ loeng08/script.js
@@ -0,0 +1,14 @@
+console.log('tere maailm!');
+console.log(123+321);
+
+let eesnimi = 'Kaspar' ;
+let perenimi = 'Kaasik';
+
+function tervitus(eesnimi, perenimi) {
+    return eesnimi + ' ' + perenimi;
+}
+
+let vastus = tervitus ('Peeter', 'Kask');
+console.log(vastus);
+console.log(eesnimi);
+console.log(perenimi);
